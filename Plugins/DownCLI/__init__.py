96e68dc5dd08313b6d445c4589d35afd000a7b47
Luka Mamukashvili <57063715+UltraStudioLTD@users.noreply.github.com>
2021-06-20 19:08:42 +0400

    Create __init__.py

---
+++ Plugins/DownCLI/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# __init__.py
+"""
+Top-level package for DownCLI-Plugin
+"""
+
+__package__ = "DownCLI-Plugin"
+__version__ = "0.0.1"
+__package_link__ = "GitHub: https://www.github.com/UltraStudioLTD/DownCLI"
+__description__ = "File Downloader"
+__author__ = "Luka Mamukashvili (GitHub: UltraStudioLTD)"
+__author_links__ = {
+    "GitHub": "https://www.github.com/UltraStudioLTD",
+    "Dev": "https://www.dev.to/ultrastudio",
+}
