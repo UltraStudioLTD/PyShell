c0920c77b16980168ccc1b01741c55033fc48f04
Luka Mamukashvili <57063715+UltraStudioLTD@users.noreply.github.com>
2021-06-20 19:07:39 +0400

    Update __init__.py

--- Plugins/Als/__init__.py
+++ Plugins/Als/__init__.py
@@ -1,19 +1,17 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # __init__.py
 """
-Top-level package for Als
+Top-level package for Als-Plugin
 """
 
-__package__ = "Als"
-__version__ = "0.0.1-alpha"
-__packagelinks__ = """
-    "GitHub": "https://www.github.com/UltraStudioLTD/Als"
-    """
+__package__ = "Als-Plugin"
+__version__ = "0.0.1"
+__package_link__ = "GitHub: https://www.github.com/UltraStudioLTD/Als"
 __description__ = "Python based Advanced version of Bash's \"ls\" command"
 __author__ = "Luka Mamukashvili (GitHub: UltraStudioLTD)"
-__authorlinks__ = """
+__author_links__ = {
     "GitHub": "https://www.github.com/UltraStudioLTD",
     "Dev": "https://www.dev.to/ultrastudio",
     "Twitter": "https://www.twitter.com/3drx6FrkfCtpyHI",
     "Instagram": "https://www.instagram.com/blue_reaper_gaming"
-    """
+}
